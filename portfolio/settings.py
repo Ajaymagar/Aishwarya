672540f9b40db2056cdf6e8ba821f4db06d7dbcc
ajay <ajay@localhost.localdomain>
2020-12-28 16:32:09 +0530

    final

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -113,7 +113,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR , '/staticfiles/')
+STATIC_ROOT = os.path.join(BASE_DIR , 'staticfiles')
 
 #DISABLE_COLLECTSTATIC=1
 
