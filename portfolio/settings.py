f81369d738ac7298c30c6932519070e2aec7e29c
ajay <ajay@localhost.localdomain>
2020-12-28 15:39:01 +0530

    final

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -114,7 +114,8 @@ STATIC_ROOT = os.path.join(BASE_DIR , 'staticfiles')
 
 
 STATICFILES_DIRS = [
-	os.path.join(BASE_DIR  , 'static')          # root folder me static folder banno 
+	os.path.join(BASE_DIR  , 'static') ,
+    '/portfolio/static/', 
 ]
 
 
